2c004b196c1adff966af29ba8a1cedf603037445
Pedro Vieira <pedrohpvieira@outlook.com>
2017-03-23 16:27:08 -0400

    Add project files.

---
+++ Financeiro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Financeiro.Startup))]
+namespace Financeiro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
