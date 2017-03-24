5ec4b640e0a018c7bc02cda850e8eb5657d9fdc7
Pedro Vieira <pedrohpvieira@outlook.com>
2017-03-24 11:28:31 -0400

    Criacao empresas controller actions

--- Financeiro/Controllers/EmpresasController.cs
+++ Financeiro/Controllers/EmpresasController.cs
@@ -1,4 +1,5 @@
 ﻿using Financeiro.Models;
+using Financeiro.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,6 +30,64 @@ namespace Financeiro.Controllers
             return View();
         }
 
-        
+        public ActionResult Edit(int id)
+        {
+            var empresa = _context.Empresas.SingleOrDefault(c => c.Id == id);
+
+            if (empresa == null)
+                return HttpNotFound();
+
+            var empresaViewModel = new EmpresaFormViewModel(empresa);
+
+            return View("EmpresaForm", empresaViewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var empresa = _context.Empresas.SingleOrDefault(c => c.Id == id);
+
+            if (empresa == null)
+                return HttpNotFound();
+
+            return View(empresa);
+        }
+
+        public ActionResult New()
+        {
+            var empresaViewModel = new EmpresaFormViewModel();
+
+            return View("EmpresaForm", empresaViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(Empresa empresa)
+        {
+            var errors = ModelState.Values.SelectMany(v => v.Errors);
+
+            if (!ModelState.IsValid)
+            {
+                return View("EmpresaForm");
+            }
+
+            if (empresa.Id == 0)
+            {
+                empresa.DataCadastro = DateTime.Now;
+                empresa.DataAtualizacao = DateTime.Now;
+
+                _context.Empresas.Add(empresa);
+            }
+            else
+            {
+                var empresaInDb = _context.Empresas.Single(c => c.Id == empresa.Id);
+
+                empresaInDb.Nome = empresa.Nome;
+                empresaInDb.DataAtualizacao = DateTime.Now;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Empresas");
+        }
     }
 }
---
+++ Financeiro/ViewModels/EmpresaFormViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Financeiro.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Financeiro.ViewModels
+{
+    public class EmpresaFormViewModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime DataAtualizacao { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                return Id != 0 ? "Editar Empresa" : "Nova Empresa";
+            }
+        }
+
+        public EmpresaFormViewModel()
+        {
+            Id = 0;
+        }
+
+        public EmpresaFormViewModel(Empresa empresa)
+        {
+            this.Id = empresa.Id;
+            this.Nome = empresa.Nome;
+        }
+    }
+}
